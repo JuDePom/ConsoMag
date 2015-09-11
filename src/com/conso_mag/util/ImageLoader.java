7fd8652228e2cc3097d723a0c7dd619ffff548c1
JuDePom <jimmy.etienne@aliceadsl.fr>
2015-09-11 21:05:07 +0200

    Loading image
    Replace the ic_launcher loading image by a new image

--- src/com/conso_mag/util/ImageLoader.java
+++ src/com/conso_mag/util/ImageLoader.java
@@ -49,7 +49,7 @@ public class ImageLoader {
     }
      
     // default image show in list (Before online image download)
-    final int stub_id=R.drawable.ic_launcher;
+    final int stub_id=R.drawable.loading_image;
      
     public void DisplayImage(String url, ImageView imageView)
     {
