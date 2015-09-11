3f72b4499e80d9db8702f2255a08d98a125c8071
JuDePom <jimmy.etienne@aliceadsl.fr>
2015-09-11 21:02:16 +0200

    About_update
    Added:
    conso-mag contact to the page
    Removed:
    test ads

--- src/com/conso_mag/AboutActivity.java
+++ src/com/conso_mag/AboutActivity.java
@@ -1,32 +1,19 @@
 package com.conso_mag;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 public class AboutActivity extends AppCompatActivity{
-	private AdView mAdView;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
-		
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-		/*
-		mAdView = (AdView) findViewById(R.id.pub_about);
-		AdRequest adRequest = new AdRequest.Builder()
-				.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-				.addTestDevice("0123456789ABCDEF")
-				.build();
-		mAdView.loadAd(adRequest);
-		*/
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
